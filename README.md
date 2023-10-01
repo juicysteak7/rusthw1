10a701f5aa5b65230dd4a2b643ecf0d9de9ef8d2
juicysteak7 <pedro.gonzalez6461@gmail.com>
2023-09-30 19:31:34 -0700

    adding notes and additional tests

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # rusthw1
 
-Pedro Gonzalez  
-Portland State University  
-CS410/510  
+Pedro Gonzalez
+
+Portland State University
+
+CS410/510
+
 Professor Bart Massey
 
 ## Background
@@ -36,3 +39,29 @@ Your program should print 16 on standard output in this case.
 Your program should accept only non-negative x and y and positive m, with at least one of x and y positive: all inputs should be less than 2**64. If the program is run with one or more invalid inputs it should print a usage message on stderr and stop with exit status 1.
 
 (Yes, there is a Rust crate with an implementation of this algorithm. You may use it for testing purposes [although I cannot recommend it, as it has some bugs], but do not look at its source code until you are done with yours.)
+
+## Project Description
+
+The professor suggested looking at our textbook, Rust Programming, for an implementation of gcd that might help with this assignment.
+
+The implementation showed how to declare functions with arguments and return values, assert something ( this is mainly used for testing but can be used otherwise ), while loops, and if statements.
+
+The book also covers how to handle command line arguments, print statements, vectors ( fancy arrays ), mutable variables, and exitig a program.
+
+I used ChatGPT for suggestions on how to handle overflows which lead me to the checked arithmetic functions.
+
+Using all this knowledge the implementation of this algorithm was fairly straitforward.
+
+## Testing
+
+The first thing I did to test the program was use the test cases the professor gave us.
+
+After I had those tests I thought of basic acceptance cases, starting with the case given to use, the inputs 2 20 and 17 should give us 16, and if making sure if m is 1 the result is 0.
+
+I thought of some different edge cases, if x or y is 0.
+
+I thought of cases that should fail. This lead me to research how to expect something to fail in rust which I was delighted to know it is handled nicely.
+
+The other cases I thought of were numbers that were too large, and if m was 0.
+
+I tried to make a test that passes a negative value into the function, but because Rust is a strongly typed language it the compiler said "oh no you cannot do that". Passing a negative argument via the command line closes the program with an appropriate error message.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+//! Command-line modular exponentation tool
+//!
+//! Pedro Gonzalez
+//! 2023
+
 use std::env;
 use std::str::FromStr;
 
 fn main() {
+    // Read arguments from environment
     println!("Starting program...");
     if env::args().len() < 4 {
         eprintln!("Must use 3 arguments.");
         error();
     }
+
+    // Load arguments into new vector
     let mut numbers = Vec::new();
     println!("Processing arguments...");
     for arg in env::args().skip(1){
-        numbers.push(u64::from_str(&arg).expect("Error parsing argument."));
+        numbers.push(u64::from_str(&arg).expect("modexp: usage: modexp <x:u64> <y:u64> <m:u64>"));
     }
+
+    // Pass arguments to modexp function
     println!("Recived arguments...");
     println!("Result: {}",modexp(numbers[0],numbers[1],numbers[2]));
 }
@@ -23,13 +33,20 @@ fn modexp(x:u64,y:u64,m:u64) -> u64 {
     let mut x:u128 = x as u128;
     let m:u128 = m as u128;
 
+    // Overflow check
     if (m-1).checked_mul(m-1).is_none() {
         eprintln!("m overflow caught.");
         error();
     }
 
+    if m == 0 {
+        error();
+    }
+
+
     while y > 0 {
         if y % 2 == 1{
+            // Overflow check
             match z.checked_mul(x) {
                 Some(result) => {
                     z = result % m;
@@ -41,6 +58,7 @@ fn modexp(x:u64,y:u64,m:u64) -> u64 {
             }
         }
         y /= 2 ;
+        // Overflow check
         match x.checked_mul(x) {
             Some(result) => {
                 x = result % m;
@@ -51,6 +69,8 @@ fn modexp(x:u64,y:u64,m:u64) -> u64 {
             }
         }
     }
+
+    // Make sure z is small enough to fit as u64.
     if z > u64::MAX as u128 {
         eprintln!("z is larger than u64 max.");
         error();
@@ -64,16 +84,56 @@ fn error() -> ! {
     std::process::exit(1);
 }
 
-#[test]
-fn test_modexp() {
-    // Largest prime less than 2**64.
-    // https://primes.utm.edu/lists/2small/0bit.html
-    let bigm = u64::max_value() - 58;
-    assert_eq!(0, modexp(bigm - 2, bigm - 1, 1));
-    assert_eq!(1, modexp(bigm - 2, bigm - 1, bigm));
-    assert_eq!(827419628471527655, modexp(bigm - 2, (1 << 32) + 1, bigm));
-    // https://practice.geeksforgeeks.org/problems/
-    //    modular-exponentiation-for-large-numbers/0
-    assert_eq!(4, modexp(10, 9, 6));
-    assert_eq!(34, modexp(450, 768, 517));
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Tests from professor
+    #[test]
+    fn test_modexp() {
+        // Largest prime less than 2**64.
+        // https://primes.utm.edu/lists/2small/0bit.html
+        let bigm = u64::max_value() - 58;
+        assert_eq!(0, modexp(bigm - 2, bigm - 1, 1));
+        assert_eq!(1, modexp(bigm - 2, bigm - 1, bigm));
+        assert_eq!(827419628471527655, modexp(bigm - 2, (1 << 32) + 1, bigm));
+        // https://practice.geeksforgeeks.org/problems/
+        //    modular-exponentiation-for-large-numbers/0
+        assert_eq!(4, modexp(10, 9, 6));
+        assert_eq!(34, modexp(450, 768, 517));
+    }   
+
+    #[test]
+    fn test_modexp_basic() {
+        // Test with prime number m=17
+        assert_eq!(modexp(2, 20, 17), 16);
+        
+        // Test with m=1 (should return 0)
+        assert_eq!(modexp(10, 5, 1), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_modexp_large_numbers() {
+        // Test with large numbers
+        assert_eq!(modexp(123456789, 987654321, 101), 100);
+    }
+
+    #[test]
+    fn test_modexp_edge_cases() {
+        // Test with edge cases
+        assert_eq!(modexp(0, 5, 17), 0); // x=0 should return 0
+        assert_eq!(modexp(5, 0, 17), 1); // y=0 should return 1
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_modexp_invalid_input() {
+        // Test with invalid input
+        assert_eq!(modexp(2, 20, 0), 0); // m=0 should return 0 (invalid)
+        assert_eq!(modexp(2, 20, 1), 0); // m=1 should return 0 (invalid)
+        assert_eq!(modexp(2, 20, std::u64::MAX), 0); // m=max should return 0 (invalid)
+    }
 }
